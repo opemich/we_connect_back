a38ccd75a38e7ca4ab96d568a146b41567c2ec96
Priest ☦ <onaopemipomichael1999@gmail.com>
2025-06-17 10:43:04 +0100

    jght

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ dotenv.config();
 const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 const cors = require("cors");
-app.use(cors());
 app.use(morgan('dev'));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-// Load environment variables from .env file
-// Set the port from environment variables or default to 5000
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 
 const port = process.env.PORT || 5000;
 // Middleware to handle CORS
